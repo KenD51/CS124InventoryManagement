b4b69aeae5bced95886e891eb6f7eb94fc6f4b87
Kenshin Drona <kenshin.drona51@gmail.com>
2026-04-29 15:49:39 -0700

    Refactor comments in restockManager.cpp
    Updated comments for clarity and fixed minor issues.

--- restockManager.cpp
+++ restockManager.cpp
@@ -4,6 +4,7 @@
 RestockTask::RestockTask(const std::string &id, const std::string &itemName, int quantityNeeded, int urgencyScore)
     : id(id), itemName(itemName), quantityNeeded(quantityNeeded), urgencyScore(urgencyScore) {}
 
+//Getters and setters for the object.
 const std::string &RestockTask::getId() const { return id; }
 const std::string &RestockTask::getItemName() const { return itemName; }
 int RestockTask::getQuantityNeeded() const { return quantityNeeded; }
@@ -25,12 +26,12 @@ bool RestockManager::isEmpty() const {
   return restockQueue.empty();
 }
 
-//Urgency is how many items are missing from target quantity;
+//Urgency is how many items are missing from the target quantity.
 int RestockManager::calculateUrgency(const Item &item, int targetQuantity) const {
     return targetQuantity - item.getQuantity;
 }
 
-//Restock Task will check will check if an item needs items, then it calculates the urgency add pushes a new object in queue with urgency factored in.
+//Restock Task will check if an item needs items, then it calculates the urgency and pushes a new object in the queue with urgency factored in.
 void RestockManager::addRestockTask(const Item &item, int targetQuantity) {
     int needed = targetQuantity - item.getQuantity();
     if (needed > 0) {
@@ -39,7 +40,7 @@ void RestockManager::addRestockTask(const Item &item, int targetQuantity) {
     }
 }
 
-//We "process" an object in queue by peeking at it, than we can pop it.
+//We "process" an object in the queue by peeking at it, then we can pop it.
 RestockTask RestockManager::popNextRestockTask() {
     RestockTask topTask = restockQueue.top();
     restockQueue.pop();
@@ -50,7 +51,16 @@ RestockTask RestockManager::peekNextRestockTask() const{
     return restockQueue.top();
 }
 
+//Traversal will return the current list as a vector.
 std::vector<RestockTask> RestockManager::generatePriorityList() const {
+    std::vector<RestockTask> list;
+    std::priorityQueue<RestockTask> temp = restockQueue;
 
+    while(!temp.empty()){
+        list.push_back(temp.top());
+        temp.pop();
+    }
+
+    return list;
 }
 
