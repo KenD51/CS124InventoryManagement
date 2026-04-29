298f8e7d33ccaec61fb1a80c56ce217850a40fb8
Kenshin Drona <kenshin.drona51@gmail.com>
2026-04-28 17:56:31 -0700

    LinkedList.h

---
+++ linkedList.h
@@ -0,0 +1,43 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include "ItemType.h"
+#include <iostream>
+
+/**
+ * @brief Node structure for the linked list.
+ */
+struct Node {
+    Item data;
+    Node* next;
+    
+    // Constructor
+    Node(const Item& item) : data(item), next(nullptr) {}
+};
+
+/**
+ * @class LinkedList
+ * @brief A simple singly linked list to store inventory items.
+ */
+class LinkedList {
+private:
+    Node* head;
+
+public:
+    LinkedList(){
+        head == nullptr;
+    }
+    ~LinkedList(); 
+
+    /**
+     * @brief Inserts a new item at the end of the list.
+     */
+    void insert(const Item& item);
+
+    /**
+     * @brief Displays all items in the linked list.
+     */
+    void display() const;
+};
+
+#endif // LINKED_LIST_H
