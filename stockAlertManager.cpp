4d1fc5c75e0677608ffceb1b9c2d2a817a0797a4
Kenshin Drona <kenshin.drona51@gmail.com>
2026-04-29 15:30:26 -0700

    Refactor constructors in StockAlert and StockAlertManager
    Refactor StockAlert and StockAlertManager constructors for initialization.

--- stockAlertManager.cpp
+++ stockAlertManager.cpp
@@ -1,38 +1,34 @@
 #include "stockAlertManager.h"
 
-StockAlert::StockAlert(const std::string &id, const std::string &itemName, int currentQuantity, int threshold) {
+// --- StockAlert Implementation ---
 
-}
+StockAlert::StockAlert(const std::string &id, const std::string &itemName, int currentQuantity, int threshold) 
+    : id(id), itemName(itemName), currentQuantity(currentQuantity), threshold(threshold) {}
 
 int StockAlert::getCurrentQuantity() const {
-
+  return CurrentQuantity;
 }
 
 const std::string& StockAlert::getId() const {
-
+  return id;
 }
 
 const std::string& StockAlert::getItemName() const {
-
+  return itemName;
 }
 
 int StockAlert::getThreshold() const {
-
+  return threshold;
 }
 
-StockAlertManager::StockAlertManager() {
+// --- StockAlertManager Implementation ---
+StockAlertManager::StockAlertManager() {}
+StockAlertManager::~StockAlertManager() {}
 
-}
-
-StockAlertManager::~StockAlertManager() {
-
-}
 std::vector<StockAlert> StockAlertManager::checkThresholds(const std::unordered_map<std::string, int>& currentInventory) const {
-
 }
 
 void StockAlertManager::setMinimumThreshold(const std::string& id, int threshold) {
-
 }
 
 int StockAlertManager::getThreshold(const std::string &id) const {
