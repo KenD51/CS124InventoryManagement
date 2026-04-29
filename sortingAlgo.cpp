2d3dddaa11a139d6a0ee796811f5867ad56d6dca
Darcie <dharxz.baculi101@gmail.com>
2026-04-28 23:55:41 -0700

    Refactor merge sort to use std::vector

--- sortingAlgo.cpp
+++ sortingAlgo.cpp
@@ -1,51 +1,50 @@
 #include sortingAlgo.h
+#include <vector>
 
-void merge(int a[], int from, int mid, int to){
-  int n = to - from + 1;
+// Merge function
+void merge(std::vector<int>& a, int from, int mid, int to) {
+    int n = to - from + 1;
+    std::vector<int> b(n);
 
-  int* b = new int[n];
+    int i1 = from;
+    int i2 = mid + 1;
+    int j = 0;
 
-  int i1 = from;
-  int i2 = mif + 1;
-
-  int j = 0;
-
-  while (i1 <=mid && 12 <= to){
-    if (a[i1] < a[i2]){
-      b[j] = a[i1];
-      i1++;
-    } else {
-      [j] = a[i2];
-      i2++;
+    while (i1 <= mid && i2 <= to) {
+        if (a[i1] < a[i2]) {
+            b[j] = a[i1];
+            i1++;
+        } else {
+            b[j] = a[i2];
+            i2++;
+        }
+        j++;
     }
-      j++;
+
+    while (i1 <= mid) {
+        b[j] = a[i1];
+        i1++;
+        j++;
     }
 
-  while (i1 <= mid){
-    b[j] = a[i1];
-    i1++;
-    j++;
-  }
+    while (i2 <= to) {
+        b[j] = a[i2];  // fixed
+        i2++;
+        j++;
+    }
 
-  while (i2 <= to){
-    b[j] = a[i1];
-    i2++;
-    j++;
-  }
-
-  for (j = 0; j < n; j++){
-    a[from + j] = b[j];
-  }
-
-  delete[] b;
-
+    for (int k = 0; k < n; k++) {
+        a[from + k] = b[k];
+    }
 }
 
-void merge_sort(int a[], int from, int to){
-  if (from == to) { return; }
-  int mid = (from + to) / 2;
+// Recursive merge sort
+void merge_sort(std::vector<int>& a, int from, int to) {
+    if (from >= to) return;
 
-  merge_sort(a, from, mid);
-  merge_sort(a, mid + 1, to);
-  merge(a, from, mid, to);
+    int mid = (from + to) / 2;
+
+    merge_sort(a, from, mid);
+    merge_sort(a, mid + 1, to);
+    merge(a, from, mid, to);
 }
