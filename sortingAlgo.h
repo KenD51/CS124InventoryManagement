556f05b9c7370afcdf70b511a511a2b0eb54a29e
Darcie <dharxz.baculi101@gmail.com>
2026-04-28 23:56:27 -0700

    Change merge and merge_sort to use std::vector

--- sortingAlgo.h
+++ sortingAlgo.h
@@ -6,13 +6,13 @@
  *  @param mid; the end of the first range
  *  @param to; the end of the second range
  */
-void merge(int a[], int from, int mid, int to);
+void merge(std::vector<int>& a, int from, int mid, int to);
 
 /**
  *  @brief sorts the elements in a range of an array
  *  @param from; start o the range to sort
  *  @param to; end of the range to sort
  */
-void merge_sort(int a[], int from, int to);
+void merge_sort(std::vector<int>& a, int from, int to);
 
 #endif
