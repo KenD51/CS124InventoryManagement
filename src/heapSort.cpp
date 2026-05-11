763d05463720d0dce6cb4656ef54664bc2451822
aaronudoutch <udoutchaaron@gmail.com>
2026-05-11 11:15:01 -0700

    core dump bug fix

--- src/heapSort.cpp
+++ src/heapSort.cpp
@@ -16,7 +16,7 @@ void fixHeapByName(std::vector<Item> &inventory, int rootIndex, int lastIndex) {
 
     while (!done) {
         int leftChildIndex = getLeftChild(index);
-        if (leftChildIndex != lastIndex) {
+        if (leftChildIndex <= lastIndex) {
             int rightChildIndex = getRightChild(index);
             // grabs child with larger alphabetical name
             if (rightChildIndex <= lastIndex && inventory[leftChildIndex].getName() < inventory[rightChildIndex].getName()) {
